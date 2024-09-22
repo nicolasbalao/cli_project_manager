f3c4e88a3fe6822d7aa7aa1dd81c7504eb060441
nicolas balao <nicolasbalao.pro@gmail.com>
2024-09-22 17:38:35 +0200

    feat(add-subcommand): Create default CLI structure and parsing arguments of subcommand add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{command, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "cli", version = "1.0", about = "Project Manager CLI")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Add {
+        path: String,
+
+        #[arg(short, long)]
+        name: Option<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add { path, name } => {
+            println!("Adding project with path: {path}");
+
+            if let Some(project_name) = name {
+                println!("Project name: {project_name}")
+            } else {
+                println!("No project name was given")
+            }
+        }
+    }
 }
