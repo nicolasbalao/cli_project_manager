0f9c5965f31e422dc385ff9988803d3e81ba581c
nicolas balao <nicolasbalao.pro@gmail.com>
2024-10-14 16:41:57 +0200

    #14 feat(run-command): Spawn shell for running command + split for getting cmd and args

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use crate::models::{project_config::ProjectConfig, project_index::ProjectIndex};
 
 pub fn execute(project_name: String, cmd: &String) {
@@ -13,5 +15,33 @@ pub fn execute(project_name: String, cmd: &String) {
 
     let project_config = ProjectConfig::load(&project_meta_data.name);
 
-    println!("Project config: {:?}", project_config.commands);
+    let cmd_exist = project_config.commands.contains_key(cmd);
+
+    if !cmd_exist {
+        eprintln!("Sorry the command is not found");
+        std::process::exit(1);
+    }
+
+    if let Some(shell_cmd) = project_config.commands.get(cmd) {
+        let (shell_cmd, args) = prepare_command_for_execution(shell_cmd);
+        println!("Command: {} Args: {:?}", shell_cmd, args);
+        let mut cmd_process = Command::new(shell_cmd)
+            .args(args)
+            .current_dir(project_config.meta_data.path)
+            .spawn()
+            .expect("Failed to spawn the processus");
+        let _ = cmd_process.wait();
+    } else {
+        eprintln!("Sorry the command is not found");
+        std::process::exit(1);
+    }
+}
+
+fn prepare_command_for_execution(cmd: &str) -> (&str, Vec<&str>) {
+    let mut cmd_parts = cmd.split_whitespace();
+
+    let command = cmd_parts.next().expect("Failed to parse command");
+    let args: Vec<&str> = cmd_parts.collect();
+
+    (command, args)
 }
